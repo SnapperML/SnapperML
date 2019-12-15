0a9cadb313e149f555dd0766da58f81a68055836
Antonio Molner Domenech <antoniomdk@hotmail.es>
2019-12-15 20:55:12 +0100

    Add utility for CLI creation

---
+++ src/utils/cli.py
@@ -0,0 +1,26 @@
+from inspect import signature
+import argparse
+
+
+def cli_decorator(func, description=''):
+    """Decorator to create a CLI around a callable.
+    It takes automatically its positional and keyword arguments
+    and generates the argument parser. """
+    parser = argparse.ArgumentParser(description=description)
+    sig = signature(func)
+
+    for parameter in sig.parameters.values():
+        name = parameter.name.replace('*', '')
+        # Not supporting *args or **kwargs arguments for now.
+        if parameter.kind != parameter.POSITIONAL_OR_KEYWORD:
+            continue
+        default = None if parameter.default == parameter.empty else parameter.default
+        if default:
+            parser.add_argument(f'--{name}', default=default)
+        else:
+            parser.add_argument(f'{name}')
+
+    def inner():
+        args = parser.parse_args()
+        return func(**vars(args))
+    return inner
