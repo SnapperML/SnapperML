f8a2cb0ed964ad524c5b918f7e13bcc20bc4e092
Antonio Molner Domenech <antoniomdk@hotmail.es>
2019-12-23 00:52:52 +0100

    Add support for using environment variables in config files

--- src/utils/config.py
+++ src/utils/config.py
@@ -2,8 +2,10 @@
 This module is for config utility functions.
 """
 import yaml
+import os
 
 
 def parse_config(config_file):
-    with open(config_file, "rb") as f:
-        return yaml.safe_load(f)
+    with open(config_file, "r") as f:
+        yaml_document = os.path.expandvars(f.read())
+        return yaml.safe_load(yaml_document)
