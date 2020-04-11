2983848b78bca8c9b8505ec513f01c57a552df39
antonio.molner <antonio.molner@strivelabs.io>
2020-04-11 21:28:58 +0200

    Add SVM model training

--- modelling/train_svm.py
+++ modelling/train_svm.py
@@ -1,80 +1,17 @@
 from typing import *
-from keras.layers import Dense, Dropout, Activation, BatchNormalization
-from keras.models import Model, Sequential
-from keras.optimizers import Adam, SGD
-from keras.callbacks import EarlyStopping
-from modelling.utils.data import UnifiedDataLoader
+from modelling.utils.data import UnifiedDataLoader, SEED
 import numpy as np
-from ml_experiment import experiment, AutologgingBackend, Trial
-from ml_experiment.integrations import KerasPruningCallback
-from modelling.utils.one_cycle import OneCycleLR
-
-VALIDATION_SPLIT = 0.2
-SEED = 1234
+from ml_experiment import experiment
+from sklearn.svm import SVC
 
 
-def create_model(output_size, layers, ps, optimizer, activation, use_bn) -> Model:
-    if not isinstance(ps, list):
-        ps = [ps] * len(layers)
-
-    model = Sequential()
-
-    for i, (units, p) in enumerate(zip(layers, ps)):
-        model.add(Dense(units))
-        if use_bn:
-            model.add(BatchNormalization())
-        model.add(Activation(activation=activation))
-        model.add(Dropout(rate=p))
-
-    model.add(Dense(output_size, activation='sigmoid'))
-    model.compile(optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-    return model
-
-
-@experiment(autologging_backends=AutologgingBackend.KERAS, data_loader=UnifiedDataLoader)
-def main(layers: List[int],
-         epochs: int,
-         batch_size: int = 128,
-         ps: Union[List[float], float] = 0,
-         one_cycle: bool = True,
-         activation='relu',
-         use_bn=True,
-         lr: float = 1e-3):
-    """Simple autoencoder"""
+@experiment(data_loader=UnifiedDataLoader)
+def main(C: float = 1.0, kernel: str = 'rbf', degree: int = 3, gamma: Any = 'scale'):
     np.random.seed(SEED)
     X_train, X_val, y_train, y_val = UnifiedDataLoader.load_data()
-    num_classes = len(np.unique(y_train))
-
-    callbacks = [
-        EarlyStopping(patience=50, restore_best_weights=True, monitor='val_accuracy'),
-        KerasPruningCallback(Trial.get_current(), monitor='val_accuracy')
-    ]
-
-    if one_cycle:
-        optimizer = SGD(learning_rate=lr)
-        callbacks.append(OneCycleLR(lr, verbose=False))
-    else:
-        optimizer = Adam(learning_rate=lr)
-
-    model = create_model(
-        output_size=num_classes,
-        layers=layers,
-        ps=ps,
-        optimizer=optimizer,
-        activation=activation,
-        use_bn=use_bn)
-
-    history = model.fit(
-        X_train,
-        y_train,
-        epochs=epochs,
-        batch_size=batch_size,
-        shuffle=True,
-        validation_data=(X_val, y_val),
-        callbacks=callbacks,
-        verbose=0)
-
-    accuracy = np.max(history.history['val_accuracy'])
+    model = SVC(C=C, kernel=kernel, degree=degree, gamma=gamma)
+    model.fit(X_train, X_val)
+    accuracy = model.score(X_val, y_val)
     return {'val_accuracy': accuracy}
 
 
