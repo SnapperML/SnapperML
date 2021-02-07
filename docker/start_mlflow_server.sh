bac7ead51eb41f2afb9cf1a63405361c2d5cc8ef
Alberto Guillén <aguillen@ugr.es>
2021-02-07 22:49:32 +0100

    Modified script to install requirement pymsql

--- docker/start_mlflow_server.sh
+++ docker/start_mlflow_server.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+pip install pymysql
 mlflow server -h 0.0.0.0 -p ${MLFLOW_PORT:-'5000'} \
     --backend-store-uri="mysql+pymysql://${MYSQL_USER}:${MYSQL_PASSWORD}@mlflow_storage:${MYSQL_PORT:-3306}/${MYSQL_DATABASE}" \
     --default-artifact-root=${MLFLOW_ARTIFACT_STORE:-'./mlruns'}
