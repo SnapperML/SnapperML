470a847ad68c7572862feac97fba22b2d24dfc41
Alberto Guillén <aguillen@ugr.es>
2021-02-12 23:57:07 +0100

    Fixed path for entrypoint in podman

--- docker/myentrypoint.sh
+++ docker/myentrypoint.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-bash -c "./docker/wait-for mlflow_storage:${MYSQL_PORT:-3306} -- ./docker/start_mlflow_server.sh && /bin/bash"
+bash -c "./wait-for mlflow_storage:${MYSQL_PORT:-3306} -- ./start_mlflow_server.sh && /bin/bash"
